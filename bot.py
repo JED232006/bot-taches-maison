1cafe37aaefb525001a5c55854a13f92e1c7b405
JED232006 <jedidibay@gmail.com>
2025-12-21 17:18:48 +0300

    Update Telegram bot token and chat_id
    Replaced the environment variable TOKEN with a hardcoded value and updated the chat_id.

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 import requests
 import os
 
-TOKEN = os.environ.get("TOKEN_TELEGRAM")
+TOKEN = "8536945340:AAHzXOhWcjEWnvzPlT2o3xv2MRsKUM4a9V8"
 
 print("TOKEN =", TOKEN)
 
 requests.post(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
     data={
-        "chat_id": 8271320198,
+        "chat_id": 	7508339230,
         "text": "TEST MESSAGE ✅ If you see this, Telegram works."
     }
 )
